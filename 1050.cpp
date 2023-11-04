7982c6fb029fa407ada85c1d7950d0ba436fe2b9
Md. Nazmul Hossain <134858823+nazmul-1117@users.noreply.github.com>
2023-11-04 17:53:36 +0600

    Create 1050.cpp

---
+++ 1050.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define lld long long int
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T=9;
+    //while(T--){
+
+        int ddd;
+        cin>>ddd;
+
+        switch(ddd){
+        case 61: cout<<"Brasilia"<<endl; break;
+        case 71: cout<<"Salvador"<<endl; break;
+        case 11: cout<<"Sao Paulo"<<endl; break;
+        case 21: cout<<"Rio de Janeiro"<<endl; break;
+        case 32: cout<<"Juiz de Fora"<<endl; break;
+        case 19: cout<<"Campinas"<<endl; break;
+        case 27: cout<<"Vitoria"<<endl; break;
+        case 31: cout<<"Belo Horizonte"<<endl; break;
+        default: cout<<"DDD nao cadastrado"<<endl;
+        }
+    //}
+
+    
+}
