619289da7623e79ec4f7f447443d55c86b456710
Md. Nazmul Hossain <134858823+nazmul-1117@users.noreply.github.com>
2023-11-05 10:12:45 +0600

    Create 1070.cpp

---
+++ 1070.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define lld long long int
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    // int T=3;
+    // while(T--){
+
+        int a;
+        cin>>a;
+        int i=a;
+
+        for(; i < a+12;  i++){
+            if(i&1){
+                cout<<i<<endl;
+            }
+        }
+
+
+
+    // }
+    
+    
+}
