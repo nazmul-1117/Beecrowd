3ad55afcba3d0a1febe225477a888dada666a640
Md. Nazmul Hossain <134858823+nazmul-1117@users.noreply.github.com>
2023-11-05 09:35:24 +0600

    Create 1065.cpp

---
+++ 1065.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define lld long long int
+
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    // int T=3;
+    // while(T--){
+
+        int c=0;
+
+        for(int i=0; i<5; i++){
+            int a=0;
+            cin>>a;
+
+            if(a&1){
+                
+            }
+
+            else{
+                c++;
+            }
+        }
+
+
+        if(c){
+            cout<<c<<" valores pares"<<endl;
+        }
+
+    // }
+    
+    
+}
