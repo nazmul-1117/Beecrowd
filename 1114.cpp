8b3fb9cfa99cb9acd19b522848359a431af41b69
Md. Nazmul Hossain <134858823+nazmul-1117@users.noreply.github.com>
2023-12-10 19:10:39 +0600

    Create 1114.cpp

---
+++ 1114.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define lld long long int
+using namespace std;
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while(1){
+        int m, n=2002;
+        cin>>m;
+
+        if(m == n){
+            cout<<"Acesso Permitido"<<endl;
+            break ;
+        }
+
+        cout<<"Senha Invalida"<<endl;
+    }
+}
