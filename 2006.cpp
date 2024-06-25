6a80ea45c46f6fb090ad7675999eb00894cd1ca3
nazmul-1117 <nazmulonline2002@gmail.com>
2024-06-25 09:32:36 +0600

    add 2006.cpp

---
+++ 2006.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define lld long long int
+
+using namespace std;
+
+int32_t main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int16_t T, a;
+    int16_t count = 0;
+
+    cin >> T;
+
+    while(cin>>a){
+    	if(a == T) count++;
+    }
+
+    // for (int i = 0; i < 5; ++i)
+    // {
+    // 	int a;
+    // 	cin>>a;
+    // 	if(a == T) count++;
+    // }
+
+    cout << count << endl;
+
+
+
+
+
+
+
+  
+}
